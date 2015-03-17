5917bf434317c7a673d895077c7e4b5ffba94299
Ben Corlett <bencorlett@me.com>
2015-03-17 11:57:29 +1100

    Work on navigation.

--- app/index.php
+++ app/index.php
@@ -8,15 +8,40 @@
 
     <!-- Stylesheets -->
     <link href="stylesheets/app.css" rel="stylesheet">
+
   </head>
   <body>
     <header role="banner">
+
+      <!-- Logo -->
       <a href="#" class="logo">
         <?=file_get_contents(__DIR__.'/images/logo.svg')?>
       </a>
+
+      <!-- Main navigation -->
+      <nav role="navigation">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">What We Do</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </nav>
+
+      <!-- Call to action navigation -->
+      <nav class="call-to-action">
+        <ul>
+          <li><a href="https://goo.gl/maps/H5kNW" target="_blank">Find Us</a></li>
+          <li><a href="tel:+612 4970 2244">Call Us</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </nav>
+
     </header>
 
     <!-- JavaScripts -->
     <script src="javascripts/app.js"></script>
+
   </body>
 </html>
